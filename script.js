0a9a0be9621ce16f852dc7ead14965984f824f90
Nicolas Peralta <nperaltapz@gmail.com>
2025-08-20 20:57:32 -0400

    new business analyst position added

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const applications = [
   {
+    name: "  Business Analysis Specialist - Global Equities Technology, Scotiabank",
+    link: "https://jobs.scotiabank.com/job/Toronto-Business-Analysis-Specialist-Global-Equities-Technology-ON-M5H1H1/594123617/",
+    location: "Toronto, ON, CA, M5H1H1 ",
+    requisition: "231388",
+  },
+  {
     name: " Analyst Programmer, Scotiabank",
     link: "https://jobs.scotiabank.com/job/Toronto-Analyst-Programmer%2C-Scotiabank-ON-M5V2T3/594193117/",
     location: "Toronto, ON, CA, M5V2T3 ",
